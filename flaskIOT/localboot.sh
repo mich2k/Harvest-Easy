f97e0b552b4fe05322f82a3b0e82d20f80d6b5e2
mich2k <miky3848@gmail.com>
2022-11-18 12:43:21 +0100

    added localboot script, updated dockerign

---
+++ flaskIOT/localboot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# This script is run by the local boot process to start the Flask server
+
+DIR="venv"
+
+if [ -d "$DIR" ]; then
+  echo "Setting up venv in ${DIR}..."
+  python3 -m venv venv
+  source venv/bin/activate
+  echo "Installing needed requirements..."
+  pip3 install -r requirements.txt
+  echo "Set up done"
+fi
+
+gunicorn -b :5000 --access-logfile - --error-logfile - flasky:app
