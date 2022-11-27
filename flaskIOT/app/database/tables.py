b9c495d716466dc3764d0b9cd92bcaba79b83542
Theviki20110 <vincenzolapadula23@gmail.com>
2022-11-27 13:33:57 +0100

    improving database module

--- flaskIOT/app/database/tables.py
+++ flaskIOT/app/database/tables.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from .__init__ import db
 
-
 class Person():
     username = db.Column('username', db.String,
                          primary_key=True, nullable=False)
@@ -11,7 +10,7 @@ class Person():
     city = db.Column('city', db.String)
     birth_year = db.Column('birth_year', db.Integer)
     
-    def __init__(self, username, name, surname, password, city, birth_year) -> None:
+    def __init__(self, username: str, name:str, surname:str, password: str, city: str, birth_year:int):
         self.username = username
         self.name = name
         self.surname = surname
@@ -27,7 +26,6 @@ class Admin(Person, db.Model):
         super().__init__(x.username, x.name, x.surname, x.password, x.city, x.birth_year)
 
 
-# db creation
 class BinRecord(db.Model):
     __tablename__ = 'bin'
     id_record = db.Column('id_record', db.Integer, primary_key=True)
@@ -38,14 +36,17 @@ class BinRecord(db.Model):
     humidity = db.Column('humidity', db.Integer, nullable=False)
     co2 = db.Column('co2', db.Integer, nullable=False)
     riempimento = db.Column('livello_di_riempimento', db.Float, nullable=False)
+<<<<<<< HEAD
     timestamp = db.Column(db.DateTime(timezone=True),
                           nullable=False,  default=datetime.utcnow)
+=======
+    timestamp = db.Column(db.DateTime(timezone=True), nullable=False,  default=datetime.utcnow)
+>>>>>>> 037edae (improving database module)
     
     # FK
     associated_bingroup = db.Column(db.Integer, db.ForeignKey('bingroup.id'))
     
     # if no jsonObj is given, a BinRecord with fake data is created
-
     def __init__(self, status, jsonObj):
         self.id_bin = jsonObj['idbin']
         self.status = status
@@ -54,35 +55,31 @@ class BinRecord(db.Model):
         self.co2 = jsonObj['co2']
         self.riempimento = jsonObj['riempimento']
 
-
 class User(Person, db.Model):
     __tablename__ = 'user'
-    apartment_ID = db.Column(db.Integer, db.ForeignKey('apartment.id'))
-
-    def __init__(self, apartment_ID, internal_number):
-        super().__init__()
-        pass
-
+    apartment_ID = db.Column('apartment_ID',db.Integer, db.ForeignKey('apartment.id'))
+    internal_number = db.Column('internal_number', db.Integer)
+    
+    def __init__(self, p: Person, apartment_ID: int, internal_number: int):
+        super().__init__(p.username, p.name, p.surname, p.password, p.birth_year)
+        self.apartment_ID = apartment_ID
+        self.internal_number = internal_number
 
 class BinGroup(db.Model):
     __tablename__ = 'bingroup'
     id = db.Column('id', db.Integer, primary_key=True)
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> 037edae (improving database module)
     # relationship
-    
     bin_records = db.relationship('BinRecord', backref='bingroup')
-    
-    def __init__(self) -> None:
-        super().__init__()
-
-    
-
 
 class Apartment(db.Model):
     __tablename__ = 'apartment'
     
     # attributes
-    
     id = db.Column('id', db.Integer, primary_key=True)
     apartment_name = db.Column('apartment_name', db.String, nullable=False)
     city = db.Column('city', db.String, nullable=False)
@@ -92,11 +89,18 @@ class Apartment(db.Model):
     n_internals = db.Column('n_internals', db.Integer, nullable=False)
     
     # FK
-    
     associated_bingroup = db.Column(db.Integer, db.ForeignKey('bingroup.id'))
     associated_admin = db.Column(db.String, db.ForeignKey('admin.username'))
 
     # relationships
-    
     users = db.relationship('User', backref='apartment')
+    
+    def __init__(self, apartment_name: str, city: str, street: str, apartment_street_number: int, n_internals: int, associated_bingroup: int, associated_admin: str):
+        self.apartment_name = apartment_name
+        self.city = city
+        self.street = street
+        self.apartment_street_number = apartment_street_number
+        self.n_internals = n_internals
+        self.associated_bingroup = associated_bingroup
+        self.associated_admin = associated_admin
     
