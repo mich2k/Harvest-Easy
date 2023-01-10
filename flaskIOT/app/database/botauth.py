87da6459f7e0e56dcf1ede71379e64e0e627a2b3
theviki20110 <vincenzolapadula23@gmail.com>
2023-01-10 15:39:05 +0100

    up

--- flaskIOT/app/database/botauth.py
+++ flaskIOT/app/database/botauth.py
@@ -4,20 +4,20 @@ from .tables import *
 #Getters
 @database_blueprint.route('/accessAdmin/<string:uid>&<string:password>', methods=['GET'])
 def login(uid, password):
-    if db.session.query(Admin.uid == uid and Admin.password == password) is not None:
-        return True
-    return False
+    if db.session.query(Admin.uid == uid and Admin.password == password):
+        return 'True'
+    return 'False'
 
 @database_blueprint.route('/checkUsername/<string:usr>', methods=['GET'])
 def checkUsername(usr):
-    if db.session.query(Admin.uid == usr) is not None:
-        return True
-    return False
+    if db.session.query(Admin.uid == usr):
+        return 'True'
+    return 'False'
 
 @database_blueprint.route('/checkSession/<string:chatid>&<string:userid>', methods=['GET'])
 def checkSession(chatid, userid):
-    if db.session.query(TelegramIDChatUser.id_user == userid and TelegramIDChatAdmin.id_user == userid) is not None:
-        return True
-    return False
+    if db.session.query(TelegramIDChatUser.id_user == userid and TelegramIDChatAdmin.id_user == userid):
+        return 'True'
+    return 'False'
 
 #Setters
