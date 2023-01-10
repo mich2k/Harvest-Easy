4ae2d220517bfe51db27df27b4b1b8f75036a3d7
mich2k <miky3848@gmail.com>
2023-01-10 01:44:17 +0100

    init login page

--- nextjs-fe-app/next-app/src/pages/index.tsx
+++ nextjs-fe-app/next-app/src/pages/index.tsx
@@ -4,82 +4,94 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <section className="h-full gradient-form bg-gray-200 md:h-screen">
+    <div className="container py-12 px-6 h-full">
+      <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+        <div className="xl:w-10/12">
+          <div className="block bg-white shadow-lg rounded-lg">
+            <div className="lg:flex lg:flex-wrap g-0">
+              <div className="lg:w-6/12 px-4 md:px-0">
+                <div className="md:p-12 md:mx-6">
+                  <div className="text-center">
+                    <img
+                      className="mx-auto w-48"
+                      width="100px"
+                      src="https://e7.pngegg.com/pngimages/165/760/png-clipart-s-s-c-napoli-2017-audi-cup-stadio-san-paolo-football-uefa-champions-league-football-ssc-napoli-2017-audi-cup.png"
+                      alt="logo"
+                    />
+                    <h4 className="text-xl font-semibold mt-1 mb-8 pb-1">the smartest bin among the smartest.</h4>
+                    <h5 className="text-l font-italic mt-1 mb-8 pb-1">Bins: semplified, purified, what else?</h5>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+                  </div>
+                  <form>
+                    <p className="mb-4">Please access with given credentials</p>
+                    <div className="mb-4">
+                      <input
+                        type="text"
+                        className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        id="exampleFormControlInput1"
+                        placeholder="Admin username"
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <input
+                        type="password"
+                        className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        id="exampleFormControlInput1"
+                        placeholder="Master password"
+                      />
+                    </div>
+                    <div className="text-center pt-1 mb-12 pb-1">
+                      <button
+                        className="inline-block px-6 py-2.5 text-gray font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-400 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
+                        type="button"
+                        data-mdb-ripple="true"
+                        data-mdb-ripple-color="light"
+                      >
+                        Log in
+                      </button>
+                      <a className="text-gray-500" href="#!">Tutto apposto?</a>
+                    </div>
+                    { /** 
+                    <div className="flex items-center justify-between pb-6">
+                      <p className="mb-0 mr-2">Don't have an account?</p>
+                      <button
+                        type="button"
+                        className="inline-block px-6 py-2 border-2 border-red-600 text-red-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                        data-mdb-ripple="true"
+                        data-mdb-ripple-color="light"
+                      >
+                        Danger
+                      </button>
+                    </div>
+                  */ }
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+                  </form>
+                </div>
+              </div>
+              <div
+                className="lg:w-6/12 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none"
+              >
+                <div className="text-black px-4 py-6 md:p-12 md:mx-6">
+                  <h4 className="text-xl font-semibold mb-6">We are more than just a bin, let me explain y.</h4>
+                  <p className="text-sm">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. 
+                    
+                    Spiega che roba è.
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                    Bidone fantastico bellissimo me lo sposo guarda eccomi ciao.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      </div>
     </div>
+  </section>
   )
 }
 
--- nextjs-fe-app/next-app/tailwind.config.js
+++ nextjs-fe-app/next-app/tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
 
   ],
