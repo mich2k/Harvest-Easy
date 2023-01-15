d7ae4490e904dd2f3791dd2c7001388da7bcc3cf
theviki20110 <vincenzolapadula23@gmail.com>
2023-01-16 00:39:33 +0100

    up utils

--- flaskIOT/app/utils/utils.py
+++ flaskIOT/app/utils/utils.py
@@ -3,7 +3,7 @@ from flask import Response
 from os import getenv
 from ..database.tables import *
 from ..trap.trap import *
-import random, requests, json
+import random, requests
 
 class Utils:
         
@@ -14,7 +14,7 @@ class Utils:
         #self.key = getenv['POST_SECRET_KEY']
         self.key = "maybesupersecretkey"
 
-    def calcolastatus(self, db, id_bin, riempimento, roll, pitch, co2):
+    def calcolastatus(self, id_bin, riempimento, roll, pitch, co2):
 
         soglie = {"plastica": 0.9, "carta": 0.9,
                   "vetro": 0.8, "umido": 0.7}  # soglie fisse
@@ -32,14 +32,13 @@ class Utils:
                 BinRecord.timestamp.desc()).first()).status
 
         tipologia = (Bin.query.filter(Bin.id_bin == id_bin)).first().tipologia
+        apartment_ID = (Bin.query.filter(Bin.id_bin == id_bin)).first().apartment_ID
         soglia_attuale = 0
 
         if (tipologia == "umido"):
-            now = datetime.datetime.now()
+            now = datetime.now()
             mese = now.month
             if (mese >= 4 and mese <= 10):  # mesi caldi
-                apartment_ID = (Bin.query.filter(
-                    Bin.id_bin == id_bin)).first().apartment_ID
                 lat = (Apartment.query.filter(
                     Apartment.apartment_name == apartment_ID)).first().lat
                 lon = (Apartment.query.filter(
@@ -69,9 +68,9 @@ class Utils:
 
                 timestamp = Bin.query.filter(
                     Bin.id_bin == id_bin).first().ultimo_svuotamento
-                last_date = datetime.datetime.strptime(
+                last_date = datetime.strptime(
                     timestamp, "%Y-%m-%d %H:%M:%S")
-                now = datetime.datetime.now()
+                now = datetime.now()
 
                 # temperature alte + sono passo più di deltagiorni
                 if ((now-last_date).days > dd_time and dd_time > 0):
@@ -94,30 +93,30 @@ class Utils:
         """
 
         # TODO rendere più fine
-        if (riempimento != None):
+        if(riempimento!=None):
             # passaggio da pieno a non pieno e viceversa
             if (status_attuale == 1 and float(riempimento) >= soglia_attuale):
-                full_state()
+                full_state(apartment_ID, riempimento)
                 status_attuale = 2
 
             if (status_attuale == 3 and float(riempimento) >= soglia_attuale):
-                full_state()
+                full_state(apartment_ID, riempimento)
                 status_attuale = 4
 
             if (status_attuale == 2 and float(riempimento) < soglia_attuale):
                 status_attuale = 1
                 db.session.query(Bin).filter(Bin.id_bin == id_bin).update(
-                    {'ultimo_svuotamento': datetime.datetime.now()})
+                    {'ultimo_svuotamento': datetime.now()})
                 db.session.commit()
 
             if (status_attuale == 4 and float(riempimento) < soglia_attuale):
                 status_attuale = 3
                 db.session.query(Bin).filter(Bin.id_bin == id_bin).update(
-                    {'ultimo_svuotamento': datetime.datetime.now()})
+                    {'ultimo_svuotamento': datetime.now()})
                 db.session.commit()
 
         # passaggio da accappottato a dritto e viceversa
-        if (roll != None and pitch != None):
+        if(roll!=None and pitch!=None):
             if (status_attuale == 3 and (roll < 45 and (abs(pitch-90) < 45))):
                 status_attuale = 1
 
@@ -131,7 +130,7 @@ class Utils:
             if (status_attuale == 2 and (roll >= 45 or (abs(pitch-90) >= 45))):
                 overturn()
                 status_attuale = 4
-        if (co2 != None):
+        if(co2!=None):
             # Caso in cui nel bidone ci dovesse essere un incendio
             if (status_attuale == 3 and co2 < limite_co2):
                 status_attuale = 1
@@ -149,12 +148,9 @@ class Utils:
 
         return status_attuale
 
-
     def set_previsione_status(id_bin, status_previsto):
-        db.session.query(Bin).filter(Bin.id_bin == id_bin).update(
-            {'previsione_status': status_previsto})
+        db.session.query(Bin).filter(Bin.id_bin == id_bin).update({'previsione_status': status_previsto})
         db.session.commit()
-
 
     def getstringstatus(status):
         if (status == 1):
@@ -167,6 +163,7 @@ class Utils:
             return "manomesso e pieno"
         else:
             return "Error"
+
     
     def randomTime(rdm = True):        
         # (24*60*60) = 86400 H/M/S
