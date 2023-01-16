b4c6ba3d16cddf86845eb6e785a5289e0110c795
theviki20110 <vincenzolapadula23@gmail.com>
2023-01-16 01:11:47 +0100

    fix prophet, db

--- flaskIOT/app/fbprophet/fbprophet.py
+++ flaskIOT/app/fbprophet/fbprophet.py
@@ -7,7 +7,6 @@ from prophet import Prophet
 import matplotlib.pyplot as plt
 import csv
 from app.utils.utils import Utils
-from prophet.plot import plot_plotly, plot_components_plotly
 
 fbprophet_blueprint = Blueprint(
     'fbprophet', __name__, template_folder='templates', url_prefix='/pred')
@@ -15,70 +14,67 @@ fbprophet_blueprint = Blueprint(
 
 @fbprophet_blueprint.route('/')
 def main():
-    return '<h1>Prophet</h1>'
+    return '<h1>FbProphet</h1>'
 
 
 @fbprophet_blueprint.route('/getprevision')
-def getprevision():
-    
-    timestamps = []
-    filling = []
-    
+def getprevision():    
     for bin in Bin.query.all():  # per ogni bidone creo una previsione temporale
+        bin_records = BinRecord.query.filter(BinRecord.associated_bin == bin.id_bin).order_by(BinRecord.timestamp.desc())[:30]
+        apartment_name = Bin.query.filter(Bin.id_bin == bin.id_bin)[0].apartment_ID
+        tipologia = Bin.query.filter(Bin.id_bin == bin.id_bin).first().tipologia
         
-        bin_records = BinRecord.query.filter(
-            BinRecord.associated_bin == bin.id_bin).order_by(BinRecord.timestamp.desc())[:30]
-        
-        apartment_name = Bin.query.filter(
-            Bin.id_bin == bin.id_bin)[0].apartment_ID
-        
-        tipologia = Bin.query.filter(
-            Bin.id_bin == bin.id_bin).first().tipologia
-                
-        for bin_record in bin_records:
-            timestamps.append(bin_record.timestamp)
-            filling.append(bin_record.riempimento)
-        
-        with open('fillinglevel.csv', 'w') as csvfile:
-            filewriter = csv.writer(csvfile, delimiter=',',
-                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            filewriter.writerow(["ds", "y"])
-            for tp, fl in zip_longest(timestamps, filling):
-                filewriter.writerow([tp, fl])
+        if(len(bin_records)>=2): 
+            timestamps = []
+            filling = []      
+            for bin_record in bin_records:
+                timestamps.append(bin_record.timestamp)
+                filling.append(bin_record.riempimento)
+            
+            with open('./app/fbprophet/predictions/%s/riempimento_%s.csv' % (apartment_name, tipologia), 'w') as csvfile:
+                filewriter = csv.writer(csvfile, delimiter=',',
+                                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                filewriter.writerow(["ds", "y"])
+                for tp, fl in zip_longest(timestamps, filling):
+                    filewriter.writerow([tp, fl])
 
-        df = pd.read_csv('fillinglevel.csv')
-        
-        df.columns = ['ds', 'y']
-        df['ds'] = pd.to_datetime(df['ds'])
+            df = pd.read_csv('./app/fbprophet/predictions/%s/riempimento_%s.csv' % (apartment_name, tipologia))
+            
+            df.columns = ['ds', 'y']
+            df['ds'] = pd.to_datetime(df['ds'])
 
-        m = Prophet()
-        m.fit(df)
-        future = m.make_future_dataframe(periods=5, freq='d')
-        future.tail()
-        forecast = m.predict(future)
-        forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
-        """
-        #m.plot(forecast)
-        #m.plot_components(forecast)
-        fig = plot_plotly(m, forecast)
-        plot_components_plotly(m, forecast)
-        datenow = datetime.now()
-        dateend = datenow + timedelta(days=5)
-        datestart = dateend - timedelta(days=20)
-        
-        plt.xlim([datestart, dateend])
-        plt.title("Bin/Filling Level forecast", fontsize=20)
-        plt.xlabel("Day", fontsize=20)
-        plt.ylabel("Filling level", fontsize=20)
-        
-        plt.savefig("./out/predictions/%s/%s/forecast.png" %
-                    (apartment_name, tipologia), format='png')
-        """
-        forecast.to_csv('prediction_%s_%s.csv' % (apartment_name, tipologia))
-        prediction = forecast[['yhat']].values
-        status_previsto = Utils.calcolastatus(
-            bin.id_bin, prediction[0], None, None, None)
-        
-        Utils.set_previsione_status(bin.id_bin, status_previsto)
-
+            m = Prophet()
+            m.fit(df)
+            future = m.make_future_dataframe(periods=5, freq='d')
+            future.tail()
+            forecast = m.predict(future)
+            forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+            
+            days=[]
+            yhats=[]
+            for i in range(len(forecast[['ds']].values)):
+                days.append(forecast[['ds']].values[i, 0])
+                yhats.append(forecast[['yhat']].values[i, 0])
+            
+            plt.plot(days, yhats)
+            datenow = datetime.now()
+            dateend = datenow + timedelta(days=7280)
+            datestart = dateend - timedelta(days=7280)
+            
+            plt.xlim([datestart, dateend])
+            plt.title("Previsioni di riempimento %s dell'appartamento %s" %
+                        (tipologia, apartment_name))
+            plt.xlabel("Data")
+            plt.ylabel("Livello di riempimento")
+            
+            plt.savefig("Predizioni/%s/%s/Forecast.png" %
+                        (apartment_name, tipologia), format='png')
+            
+            forecast.to_csv('./app/fbprophet/predictions/%s/prediction_%s.csv' % (apartment_name, tipologia))
+            prediction = forecast[['yhat']].values
+            status_previsto = Utils.calcolastatus(
+                bin.id_bin, prediction[0], None, None, None)
+            
+            Utils.set_previsione_status(bin.id_bin, status_previsto)
+  
     return 'done'
