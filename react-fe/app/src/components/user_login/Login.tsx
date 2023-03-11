1eaffb89dfd7320a5f0bb750da995e28eec225d3
mich2k <miky3848@gmail.com>
2023-03-11 13:39:38 +0100

    up

--- react-fe/app/src/components/user_login/Login.tsx
+++ react-fe/app/src/components/user_login/Login.tsx
@@ -10,7 +10,6 @@ import { useNavigate } from "react-router-dom";
 import useToken from '../../useToken';
 const Login = () => {
 
-
   const { setToken } = useToken();
 
   const navigate = useNavigate();
