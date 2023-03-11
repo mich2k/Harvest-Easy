f066fd6751d2a0e5feec6feb90274c8f159c8844
mich2k <miky3848@gmail.com>
2023-03-11 18:29:13 +0100

    typology key header

--- react-fe/app/src/components/Base64toRenderedImages.tsx
+++ react-fe/app/src/components/Base64toRenderedImages.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 
 export interface base64Previsions {
-    [key: string]: string  }
+    [key: string]: string
+}
 
-  
+
 interface Props {
     previsioni: base64Previsions;
 }
 
+function capitalizeFirstLetter(string:string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 const ImmaginiPrevisioni: React.FC<Props> = ({ previsioni }) => {
-    if(!previsioni) return (<div className='text-bold'>Loading previsions or not avialable yet for your apartment.</div>);
+    if (!previsioni) return (<div className='text-bold'>Loading previsions or not avialable yet for your apartment.</div>);
     return (
         <div>
-            {Object.keys(previsioni).map((key:string, index) => (
-                <img key={index} src={`data:image/png;base64,${previsioni[key]}`} alt={key} />
+            {Object.keys(previsioni).map((key: string, index) => (
+                <div key={index} className='my-6'>
+                    <div className='text-center font-bold my-6'>{capitalizeFirstLetter(key)}</div>
+                    <img key={index} src={`data:image/png;base64,${previsioni[key]}`} alt={key} />
+                </div>
             ))}
         </div>
     );
