f64e64b9147cd93a78dc86ec1e5d8cbce212645d
mich2k <miky3848@gmail.com>
2023-03-14 15:29:25 +0100

    QR_CODE + refractor eng to ita due to backend compatib.

--- react-fe/app/src/components/georecord/AdminRecord.tsx
+++ react-fe/app/src/components/georecord/AdminRecord.tsx
@@ -176,25 +176,25 @@ const AdminRecord = () => {
                         <ul className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                           <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
                             <div className="flex items-center pl-3">
-                              <input onChange={() => { onCheckboxChange("paper") }} id="paper-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                              <input onChange={() => { onCheckboxChange("carta") }} id="paper-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
                               <label htmlFor="paper-checkbox" className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Paper</label>
                             </div>
                           </li>
                           <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
                             <div className="flex items-center pl-3">
-                              <input onChange={() => { onCheckboxChange("plastic") }} id="plastic-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                              <input onChange={() => { onCheckboxChange("plastica") }} id="plastic-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
                               <label htmlFor="plastic-checkbox" className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Plastic</label>
                             </div>
                           </li>
                           <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
                             <div className="flex items-center pl-3">
-                              <input onChange={() => { onCheckboxChange("glass") }} id="glass-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                              <input onChange={() => { onCheckboxChange("vetro") }} id="glass-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
                               <label htmlFor="glass-checkbox" className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Glass</label>
                             </div>
                           </li>
                           <li className="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
                             <div className="flex items-center pl-3">
-                              <input onChange={() => { onCheckboxChange("organic") }} id="organic-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                              <input onChange={() => { onCheckboxChange("umido") }} id="organic-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
                               <label htmlFor="organic-checkbox" className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Organic</label>
                             </div>
                           </li>
