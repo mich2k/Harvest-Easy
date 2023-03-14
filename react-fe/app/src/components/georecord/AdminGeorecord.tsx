e00498f02798de08acd32c7dc453de81d1ad3f18
mich2k <miky3848@gmail.com>
2023-03-14 16:07:14 +0100

    overflow adj

--- react-fe/app/src/components/georecord/AdminGeorecord.tsx
+++ react-fe/app/src/components/georecord/AdminGeorecord.tsx
@@ -156,11 +156,11 @@ const AdminGeorecord = () => {
                     return (
 
                         <section className="h-full gradient-form bg-gray-200 md:h-screen">
-                            <div className="container py-12 px-6 h-full">
-                                <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-                                    <div className="xl:w-10/12">
-                                        <div className="block bg-white shadow-lg rounded-lg">
-                                            <div className="lg:flex lg:flex-wrap g-0">
+                            <div className="overflow-auto container py-12 px-6 h-full">
+                                <div className="overflow-auto flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+                                    <div className="overflow-auto xl:w-10/12">
+                                        <div className="overflow-auto block bg-white shadow-lg rounded-lg">
+                                            <div className="overflow-auto lg:flex lg:flex-wrap g-0">
                                                 <div className="lg:w-6/12 px-4 md:px-0">
                                                     <div className="md:p-12 md:mx-6">
                                                         <div className="text-center">
