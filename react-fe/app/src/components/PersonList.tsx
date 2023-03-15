b22ab61d95f81583b362c01a48097ca00b8ba75f
mich2k <miky3848@gmail.com>
2023-03-15 18:43:56 +0100

    fix telegram

--- react-fe/app/src/components/PersonList.tsx
+++ react-fe/app/src/components/PersonList.tsx
@@ -135,7 +135,7 @@ const PersonList = (props: Props) => {
                         <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                             @
                         </span>
-                        <input onChange={(e) => setTelegramUsername(e.target.value)}
+                        <input onChange={(e) => setTelegramUsername('@' + e.target.value)}
                             value={telegramUsername}
                             type="text" id="website-admin" className="w-40 rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="easy_whistleblower" />
                     </div>
